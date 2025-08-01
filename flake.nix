cc7d839722af883901a5be58938687786d75a71a
Rafael Fernández López <ereslibre@gmail.com>
2025-08-01 08:41:34 +0200

    feat: Implement refresh_repositories tool (#5)
    Implement the ability to synchronize their local package database with remote repositories, ensuring access to the latest package information and versions.

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
       pkgs = import nixpkgs {inherit system overlays;};
     in {
       devShells.default = pkgs.mkShell {
-        buildInputs = with pkgs; [alejandra (pkgs.rust-bin.stable.latest.default.override {targets = ["x86_64-unknown-linux-musl"];}) rustfmt];
+        buildInputs = with pkgs; [alejandra (rust-bin.stable.latest.default.override {targets = ["x86_64-unknown-linux-musl"];}) rustfmt];
       };
     });
 }
